b65a40b3092bf6baf961beca2263e7508a1c13ea
Goblester <goblin829@gmail.com>
2021-07-28 19:59:31 +0300

    home photo background

--- src/n1-main/m1-UI/Header/Header.tsx
+++ src/n1-main/m1-UI/Header/Header.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import { Navigation } from '../Navigtaion/Navigation';
+import React, {useEffect, useState} from 'react';
+import {Navigation} from '../Navigtaion/Navigation';
 import styles from './Header.module.scss';
 import styleContainer from '../common/styles/styleContainer.module.scss';
+import {Links} from './Links';
+import {useSelector} from 'react-redux';
+import {AppStoreType} from '../../m2-BLL/Store';
 
-export function Header(){
-    return(
-        <header className={styles.header}>
+
+
+export function Header() {
+
+    const fixed = useSelector<AppStoreType,boolean>(state=>state.app.scrollBelow)
+
+    return (
+        <header className={`${styles.header} ${fixed?styles.fixed:styles.absolute}`}>
             <div className={`${styleContainer.container} ${styles.container}`}>
                 <Navigation/>
+                <div className={styles.links}>
+                    <Links/>
+                </div>
             </div>
         </header>
     )
