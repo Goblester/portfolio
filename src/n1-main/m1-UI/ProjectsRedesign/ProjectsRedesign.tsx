e98f085f5fbd92292a6bf53952baa8221e1238e8
Goblester <goblin829@gmail.com>
2023-09-21 20:48:14 +0300

    project redesign. render projects

--- src/n1-main/m1-UI/ProjectsRedesign/ProjectsRedesign.tsx
+++ src/n1-main/m1-UI/ProjectsRedesign/ProjectsRedesign.tsx
@@ -1,17 +1,31 @@
+import {useState} from "react";
 import sCon from './../common/styles/styleContainer.module.scss';
 import s from './ProjectRedesign.module.scss';
+import {useSelector} from "react-redux";
+import {AppStoreType} from "../../m2-BLL/Store";
+import {ProjectType} from "../../m2-BLL/projectsReducer";
+import {MouseTooltip} from "../MouseTooltip/MouseTooltip";
+import classNames from "classnames";
 
 
 export const ProjectsRedesign = () => {
 
+    const [currentProject, setCurrentProject] = useState<ProjectType | null>(null);
+    const projects = useSelector((state: AppStoreType) => state.projects);
     return (
-        <section>
+        <section onMouseLeave={() => setCurrentProject(null)}>
             <ul className={sCon.container}>
-                <li className={s.project}>
-                    <h4 className={s.projectName}>Fabric</h4>
-                    <p className={s.projectDescription}>Design development</p>
-                </li>
+                {projects.map((project, index) => (
+                    <li data-project={index}
+                        className={classNames(s.project, {[s.projectHovered]: currentProject?.title === project.title})}
+                        onMouseEnter={() => setCurrentProject(project)}
+                        key={project.title}>
+                        <h4 className={s.projectName}>{project.title}</h4>
+                        <p className={s.projectDescription}>{project.description}</p>
+                    </li>
+                ))}
             </ul>
+            {currentProject && <MouseTooltip/>}
         </section>
     )
 }
