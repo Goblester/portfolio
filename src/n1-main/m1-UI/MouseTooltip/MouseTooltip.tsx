351abb41c7eb3fd36806e597aa577f7de69b90f7
Goblester <goblin829@gmail.com>
2023-09-24 21:55:09 +0300

    custom scroll state changing

--- src/n1-main/m1-UI/MouseTooltip/MouseTooltip.tsx
+++ src/n1-main/m1-UI/MouseTooltip/MouseTooltip.tsx
@@ -1,20 +1,13 @@
-import {useMotionValue, motion, useSpring, useTransform, useVelocity} from "framer-motion";
-import React, {useEffect} from "react";
+import {motion, MotionValue, useTransform, useVelocity} from "framer-motion";
+import React from "react";
 import s from './style.module.scss';
 
-export const MouseTooltip: React.FC = () => {
+type PropsType = {
+    x: MotionValue<number>;
+    y: MotionValue<number>;
+}
 
-    const baseX = useMotionValue(0);
-    const baseY = useMotionValue(0);
-
-    const x = useSpring(baseX, {
-        damping: 50,
-        stiffness: 400
-    });
-    const y = useSpring(baseY, {
-        damping: 50,
-        stiffness: 400
-    });
+export const MouseTooltip: React.FC<PropsType> = ({x, y}) => {
 
     const velX = useVelocity(x)
     const velY = useVelocity(y)
@@ -23,14 +16,6 @@ export const MouseTooltip: React.FC = () => {
     const textY = useTransform(velY, (latest) => latest/1000 * 10);
 
 
-    useEffect(() => {
-        const updateMousePosition = (e: MouseEvent) => {
-            x.set(e.clientX);
-            y.set(e.clientY);
-        };
-        window.addEventListener('mousemove', updateMousePosition);
-        return () => window.removeEventListener('mousemove', updateMousePosition);
-    }, []);
 
 
     return (
--- src/n1-main/m1-UI/ProjectsRedesign/ProjectsRedesign.tsx
+++ src/n1-main/m1-UI/ProjectsRedesign/ProjectsRedesign.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import sCon from './../common/styles/styleContainer.module.scss';
 import s from './ProjectRedesign.module.scss';
 import {useSelector} from "react-redux";
@@ -6,15 +6,61 @@ import {AppStoreType} from "../../m2-BLL/Store";
 import {ProjectType} from "../../m2-BLL/projectsReducer";
 import {MouseTooltip} from "../MouseTooltip/MouseTooltip";
 import classNames from "classnames";
+import {useMotionValue, useSpring} from "framer-motion";
 
 
 export const ProjectsRedesign = () => {
+    const ref = useRef<HTMLUListElement | null>(null);
+    const baseX = useMotionValue(0);
+    const baseY = useMotionValue(0);
+
+    const x = useSpring(baseX, {
+        damping: 50,
+        stiffness: 400
+    });
+    const y = useSpring(baseY, {
+        damping: 50,
+        stiffness: 400
+    });
 
     const [currentProject, setCurrentProject] = useState<ProjectType | null>(null);
     const projects = useSelector((state: AppStoreType) => state.projects);
+
+    useEffect(() => {
+        const updateMousePosition = (e: MouseEvent) => {
+            x.set(e.clientX);
+            y.set(e.clientY);
+        };
+        window.addEventListener('mousemove', updateMousePosition);
+        return () => window.removeEventListener('mousemove', updateMousePosition);
+    }, []);
+
+    useEffect(() => {
+        const updateScroll = () => {
+            // @ts-ignore
+            const {height, width, top, left} = ref.current?.getBoundingClientRect() as any;
+            const mousePosition = {x: x.get(), y: y.get()};
+
+            if (top + height < mousePosition.y
+                || top > mousePosition.y
+                || width + left < mousePosition.x
+                || left > mousePosition.x) {
+                setCurrentProject(null);
+                return;
+            } else {
+                const childrenHeight = ref.current?.children[0]?.clientHeight || 0;
+                const index = Math.floor((mousePosition.y - top)  / childrenHeight);
+                console.log(index);
+                setCurrentProject(projects[index])
+            }
+        };
+        window.addEventListener('scroll', updateScroll);
+        return () => window.removeEventListener('scroll', updateScroll);
+    }, [projects]);
+
     return (
-        <section onMouseLeave={() => setCurrentProject(null)}>
-            <ul className={sCon.container}>
+        <section>
+            <ul ref={ref} className={sCon.container}>
                 {projects.map((project, index) => (
                     <li data-project={index}
                         className={classNames(s.project, {[s.projectHovered]: currentProject?.title === project.title})}
@@ -25,7 +71,7 @@ export const ProjectsRedesign = () => {
                     </li>
                 ))}
             </ul>
-            {currentProject && <MouseTooltip/>}
+            {currentProject && <MouseTooltip x={x} y={y}/>}
         </section>
     )
 }
